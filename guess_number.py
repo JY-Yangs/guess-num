5afe65e779d24726658ff5da87613a2bf8414e44
JY-Yangs <wbhyex@gmail.com>
2021-01-25 21:21:54 +0800

    add count

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,9 @@
 import random
 random_number = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count += 1 就是 count = count + 1的快速寫法
+	print('目前猜第', count, '次')
 	guess_number = int(input('請猜數字1-100: '))
 	if guess_number > random_number:
 		print('比答案大')
